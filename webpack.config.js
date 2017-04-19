75fda9babeecec4d9662fba50338b97079e054e3
dandanloveJM <gigi834921748@qq.com>
2017-04-20 00:09:45 +0800

    修改动手题目

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 var path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: {
+        new webpack.optimize.MinChunkSizePlugin({
+            compress: {
+                warnings: false //去掉warning
+            },
+            output: {
+                comments: false //去掉所以注释
+            },
+            mangle: {
+                except: ['$super', '$', 'exports', 'require'] //不会压缩含有'$super', '$', 'exports', 'require'的内容
+            }
+        })
+
+    }
 };
