a65418f06f76e5f64b63a5a6d01d705bf2e9a42e
dandanloveJM <gigi834921748@qq.com>
2017-05-10 11:01:35 +0800

    改变代码后自动刷新页面

--- dist/bundle.js
+++ dist/bundle.js
@@ -27447,7 +27447,7 @@ function component () {
   var element = __WEBPACK_IMPORTED_MODULE_1_jquery___default()('<div></div');
 
   /* lodash is required for the next line to work */
-  element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['hello','webpack'], ' '))
+  element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['hi','webpack'], ' '))
 
   return element.get(0);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    plugins: {
-        new webpack.optimize.MinChunkSizePlugin({
-            compress: {
-                warnings: false //去掉warning
-            },
-            output: {
-                comments: false //去掉所以注释
-            },
-            mangle: {
-                except: ['$super', '$', 'exports', 'require'] //不会压缩含有'$super', '$', 'exports', 'require'的内容
-            }
-        })
+    }
+   
 
     }
-};
