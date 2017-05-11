531c348e4196828f1874a5df0bff1f5c57539507
dandanloveJM <gigi834921748@qq.com>
2017-05-11 14:53:30 +0800

    state的用法

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Welcome from './Welcome'
 
 ReactDOM.render(
-  <h1>Hello,React!</h1>,
+  <Welcome/>,
   document.getElementById('box')
-);
+
+)
+
+function tick(){
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('box')
+  );
+}
+//setInterval(tick, 1000);
 
