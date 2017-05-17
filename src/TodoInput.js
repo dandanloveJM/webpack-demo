3ccd3d2f311078aeca51a05d354250565832cbdf
dandanloveJM <gigi834921748@qq.com>
2017-05-17 11:11:38 +0800

    重新安装react

--- src/TodoInput.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class TodoInput extends Component{
-    render(){
-        return <input type="text" value={this.props.content}/>
-    }
-}
-
-export default TodoInput
--- src/index.js
+++ src/index.js
@@ -2,25 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 import './index.css';
-import Welcome from './Welcome'
 
-ReactDOM.render(
-  <Welcome name="dandan"/>,
-  document.getElementById('box')
 
-)
 
-function tick(){
+function tick() {
   const element = (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
   ReactDOM.render(
     element,
-    document.getElementById('box')
+    document.getElementById('root')
   );
 }
-//setInterval(tick, 1000);
 
+setInterval(tick, 1000);
