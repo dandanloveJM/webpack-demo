b3f2b0d33185f3d74601eda081e0c25729cca94b
dandanloveJM <gigi834921748@qq.com>
2017-05-18 01:26:06 +0800

    增加了normalize.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
+import 'normalize.css'
 
 class App extends Component {
   constructor(props){
