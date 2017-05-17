5b80b37d3d0f37bfc794e2a30eaea17e347234fd
dandanloveJM <gigi834921748@qq.com>
2017-05-17 22:55:33 +0800

    搭建基本骨架

--- src/App.js
+++ src/App.js
@@ -2,5 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        {id:1, title:'第一个待办'}
+      ]
+    }
+  }
+  render() {
+    let todos = this.state.todoList.map(function(item, index){
+      return <li>{item.title}</li>
+    })
+    return (
+      <div className="App">
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value='{this.state.newTodo}'/>
+        </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default App;
